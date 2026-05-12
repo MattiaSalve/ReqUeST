4f756af7bd3e30cd98e4bc48b21ff05f0ace2712
Mattia Salvetti <mattiasalvetti@Mattias-Air-2.fritz.box>
2026-05-12 07:31:43 +0200

    Improved url list parsing

--- src/client.rs
+++ src/client.rs
@@ -3,14 +3,16 @@ use tokio;
 
 fn read_urls_from_list(f: &str) -> Result<Vec<String>, std::io::Error> {
     let contents = std::fs::read_to_string(f)?;
-    Ok(contents.split(", ").map(|s| s.to_string()).collect())
+    let re = regex::Regex::new(r"\s*,\s*").unwrap();
+    let urls: Vec<String> = re.split(&contents).map(|s| s.trim().to_string()).collect();
+    Ok(urls)
 }
 
 pub async fn get_page(url: &str) -> Result<String, reqwest::Error> {
     Ok(reqwest::get(url).await?.text().await?)
 }
 
-pub async fn get_pages(list: Vec<String>) -> Vec<Result<String>, reqwest::Error> {}
+// pub async fn get_pages(list: Vec<String>) -> Vec<Result<String>, reqwest::Error> {}
 
 #[cfg(test)]
 mod tests {
@@ -20,6 +22,18 @@ mod tests {
     fn test_read_urls_from_list() {
         let path = "test.csv";
         std::fs::write(path, "https://apple.com, https://google.com").unwrap();
+        let urls = read_urls_from_list(path);
+        assert_eq!(
+            urls.unwrap(),
+            vec!["https://apple.com", "https://google.com"]
+        );
+        std::fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_read_urls_with_newlines() {
+        let path = "test.csv";
+        std::fs::write(path, "https://apple.com   ,\n https://google.com").unwrap();
         let urls = read_urls_from_list(path);
         assert_eq!(
             urls.unwrap(),
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,4 @@ use ReqUeST::client;
 // use tokio;
 
 #[tokio::main]
-async fn main() {
-    match client::get_page("https://doc.rust-lang.org/rust-by-example/hello.html").await {
-        Ok(html) => println!("{}", html),
-        Err(e) => eprintln!("Error: {}", e),
-    }}
-}
-
+async fn main() {}
